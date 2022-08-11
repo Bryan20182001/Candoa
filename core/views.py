9e82f71b3e9993f1283163c880335e2e60815a34
Bryan20182001 <bryancando142@gmail.com>
2022-08-11 16:19:31 -0500

    Añadir Proyecto

---
+++ core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+# *----------------HERENCIA-----------------------------------------------*
+
+# def inicio(request):
+#     return render(request, 'inicio.html')
+#
+#
+# def noticias(request):
+#     return render(request, 'noticias.html')
+#
+# def farandula(request):
+#     return render(request, 'farandula.html')
+#
+#
+#
+# def quienes_somos(request):
+#     return render(request, 'quienes_somos.html')
+
+
